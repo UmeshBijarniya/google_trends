faafd5822a247c5c99d45c1cad7c97146e3c3112
Umesh Bijarniya <u961000@gmail.com>
2026-02-03 23:57:53 +0530

    testing

--- gtrends/main.py
+++ gtrends/main.py
@@ -1,60 +1,95 @@
 import os,time
-from datetime import datetime
+from datetime import datetime, timedelta
 import csv
 from chunk.fetch import SerpApiTrendsFetcher
 from process.sync import GoogleTrendSyncer
 
-fetcher = SerpApiTrendsFetcher()
+
 syncer = GoogleTrendSyncer()
 
 Api_key = "dd91f8ff20268440571b3410cec679caacbaa390ef89e238e20d5d649829f35d"
+fetcher = SerpApiTrendsFetcher(Api_key)
 
-kw_to_search = "Rajasthan"
+# kw_to_search = "Rajasthan"
+search_list = ["Jaipur"]
 window_days = 240
 overlap_days = 30
 
 start = datetime.strptime("2021-04-02", "%Y-%m-%d").strftime("%Y-%m-%d")
 end = datetime.today().strftime("%Y-%m-%d")
 
-try:
-    windows = syncer.generate_windows(start,end,window_days,overlap_days)
-    print(f"Windows generated of length:{len(windows)}")
-except Exception as e:
-    print(f"error generating window: {e}")
+for search in search_list:
+
+        safe_keyword = "".join(c if c.isalnum() or c in ("_", "-") else "_" for c in search)
+        csv_file = f"{safe_keyword}.csv"
+
+        file_exists = os.path.isfile(csv_file)
+
+        # get last date from csv
+        last_date = syncer.get_last_date(csv_file,"date")
+        if last_date:
+            print(f"fetching from the date:{last_date}")
+
+            start = (last_date - timedelta(days=overlap_days)).strftime("%Y-%m-%d")
+
+        # generate windows
+        try:
+            windows = syncer.generate_windows(start,end,window_days,overlap_days)
+            print(f"Windows generated of length:{len(windows)}")
+        except Exception as e:
+            print(f"error generating window: {e}")
 
 
-for window in windows:
+        for window in windows:
 
-    w_start, w_end = window
-    print(f"fetching window of length:{len(window)}")
+            w_start, w_end = window
+            print(f"fetching window :{w_start}->{w_end}")
 
-    try:
-        chunk = fetcher.fetch_chunk(kw_to_search,w_start,w_end,"youtube")
-        print(f"chunk fetched sucessfully")
-    except Exception as e:
-        print(f"error fetching {e}")
+            # fetch data
+            try:
+                chunk = fetcher.fetch_chunk(search,w_start,w_end,"youtube")
+                print(f"chunk fetched sucessfully")
+            except Exception as e:
+                print(f"error fetching {e}")
 
-    print(f"length of chunk: {len(chunk)}")
+            
+            # get last 30 days data from the csv file to find overlap
+            if file_exists:
+                with open(csv_file, mode="r",newline="", encoding="utf-8") as f:
+                    reader = list(csv.reader(f))
 
-    time.sleep(3)
-    import json
+                    rows = reader[1:]           # actual data rows
 
-    csv_file = "google_trends.csv"
-    file_exists = os.path.isfile(csv_file)
+                    past = rows[-30:] 
+                    if not past:
+                        past = chunk
 
-    with open(csv_file, mode="a", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
+                overlap = syncer.find_overlap(past, chunk)
+                scale = syncer.compute_scaling_factor(overlap)
+                chunk = syncer.normalize_new_data(chunk, scale)
 
-        # write header only once
-        if not file_exists:
-            writer.writerow(["date", "value"])
 
-        for row in chunk:
-            writer.writerow([
-                datetime.fromtimestamp(row["timestamp"]).strftime("%Y-%m-%d"),
-                row["value"]
-            ])
-        print(f"data saved to file successfully")
+                if rows:
+                    last_saved_date = rows[-1][0] 
+                    # Filter chunk to keep only dates after the last saved date
+                    chunk = [row for row in chunk if row["date"] > last_saved_date]
+
+            # append the data in file
+            with open(csv_file, mode="a", newline="", encoding="utf-8") as f:
+                writer = csv.writer(f)
+
+                # write header only once
+                if not file_exists:
+                    writer.writerow(["date", search])
+                    file_exists = True
+
+                for row in chunk:
+                    writer.writerow([
+                        row["date"],
+                        row["value"]
+                    ])
+
+            print(f"Data saved to {csv_file}")
 
 
 
